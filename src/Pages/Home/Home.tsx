0dcf227feaa65c6e97c922eab4e7feac65baaf67
Kasim <kasimnadim6@gmail.com>
2022-01-28 21:58:03 +0530

    icons added

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,45 @@
 import About from '../../Components/About/About';
 import Header from '../../Layouts/Header/Header';
 import styles from './Home.module.scss';
-
+import {
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  WhatsappLogo,
+} from 'phosphor-react';
 const Home = () => {
   return (
     <>
       {/* <header className={styles.header}> */}
       <Header />
       {/* </header> */}
+      <div className={styles['social-icons']}>
+        <ul>
+          <li>
+            <a href="linkedin.com" className={styles.linkedin}>
+              <LinkedinLogo weight="fill" />
+            </a>
+          </li>
+          <li>
+            <a href="whatsapp.com" className={styles.whatsapp}>
+              <WhatsappLogo weight="fill" />
+            </a>
+          </li>
+          <li>
+            <a href="github.com" className={styles.github}>
+              <GithubLogo weight="fill" />
+            </a>
+          </li>
+          <li>
+            <a href="instagram.in" className={styles.instagram}>
+              <InstagramLogo weight="fill" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.email}>
+        <a href="#">kasimnadim6@gmail.com</a>
+      </div>
       <main className={styles.main}>
         <About />
       </main>
