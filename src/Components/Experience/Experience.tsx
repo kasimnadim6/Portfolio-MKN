36c7fda1c02b2012212cb1d0d1bbb44a1d15a0e9
Kasim <kasimnadim6@gmail.com>
2022-02-09 21:21:34 +0530

    experience code update

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -4,6 +4,7 @@ const Experience = () => {
   return (
     <section id="experience" className={styles['experience']}>
       <h2 className={styles['experience-header']}>Where I've Worked</h2>
+      <div className={styles['experience-body']}></div>
     </section>
   );
 };
