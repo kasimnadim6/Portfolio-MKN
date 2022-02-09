19f20bd53ee4414bba90b7072caebc36dcc02044
Kasim <kasimnadim6@gmail.com>
2022-02-10 00:13:49 +0530

    tab created

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,10 +1,104 @@
 import styles from './Experience.module.scss';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box className={styles['tab-body']}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const Experience = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <section id="experience" className={styles['experience']}>
       <h2 className={styles['experience-header']}>Where I've Worked</h2>
-      <div className={styles['experience-body']}></div>
+      <Box className={styles['experience-body']}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: 'divider' }}
+          TabIndicatorProps={{ style: { backgroundColor: '#64ffda' } }}
+          className={styles['tab-heading-container']}
+        >
+          <Tab
+            className={styles['tab-header']}
+            label="Cognizant"
+            {...a11yProps(0)}
+          />
+          <Tab
+            className={styles['tab-header']}
+            label="Mindtree Ltd."
+            {...a11yProps(1)}
+          />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit
+          similique nobis delectus facilis ex minima porro nesciunt repellendus
+          enim placeat dolorem, molestiae accusantium ratione doloribus atque.
+          Neque, officia possimus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptate sit similique nobis delectus facilis ex
+          minima porro nesciunt repellendus enim placeat dolorem, molestiae
+          accusantium ratione doloribus atque. Neque, officia possimus.Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit
+          similique nobis delectus facilis ex minima porro nesciunt repellendus
+          enim placeat dolorem, molestiae accusantium ratione doloribus atque.
+          Neque, officia possimus.Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptate sit similique nobis delectus facilis ex
+          minima porro nesciunt repellendus enim placeat dolorem, molestiae
+          accusantium ratione doloribus atque. Neque, officia possimus.Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit
+          similique nobis delectus facilis ex minima porro nesciunt repellendus
+          enim placeat dolorem, molestiae accusantium ratione doloribus atque.
+          Neque, officia possimus.Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptate sit similique nobis delectus facilis ex
+          minima porro nesciunt repellendus enim placeat dolorem, molestiae
+          accusantium ratione doloribus atque. Neque, officia possimus.Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit
+          similique nobis delectus facilis ex minima porro nesciunt repellendus
+          enim placeat dolorem, molestiae accusantium ratione doloribus atque.
+          Neque, officia possimus.
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two Description
+        </TabPanel>
+      </Box>
     </section>
   );
 };
