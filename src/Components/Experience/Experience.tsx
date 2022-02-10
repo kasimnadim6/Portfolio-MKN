da2d46aed60ff05b5800c967c6f3c4820660703f
Kasim <kasimnadim6@gmail.com>
2022-02-10 11:57:38 +0530

    tabs responsiveness

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,10 +1,11 @@
 import styles from './Experience.module.scss';
-import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
+import { useState, useEffect } from 'react';
+import { Orientation } from '@mui/material';
+import { isMobile } from 'react-device-detect';
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -35,26 +36,37 @@ function a11yProps(index: number) {
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
-
 const Experience = () => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [orientation, setOrientation] = useState<Orientation>('vertical');
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  useEffect(() => {
+    if (isMobile) {
+      setOrientation('horizontal');
+    }
+  }, []);
   return (
     <section id="experience" className={styles['experience']}>
       <h2 className={styles['experience-header']}>Where I've Worked</h2>
-      <Box className={styles['experience-body']}>
+      <Box
+        className={`${styles['experience-body']} ${
+          orientation === 'horizontal' ? styles['experience-body-sm'] : ''
+        }`}
+      >
         <Tabs
-          orientation="vertical"
-          variant="scrollable"
+          orientation={orientation}
+          variant={orientation === 'horizontal' ? 'fullWidth' : 'scrollable'}
           value={value}
           onChange={handleChange}
           aria-label="Vertical tabs example"
           sx={{ borderRight: 1, borderColor: 'divider' }}
-          TabIndicatorProps={{ style: { backgroundColor: '#64ffda' } }}
+          TabIndicatorProps={{
+            style: { backgroundColor: '#64ffda' },
+          }}
           className={styles['tab-heading-container']}
         >
           <Tab
